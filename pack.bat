91ff3b2f64bd3dd791b7765c901bb45e48357c4e
Nepove <84743964+Nepove@users.noreply.github.com>
2023-12-12 19:21:01 -0600

    Create pack.bat

---
+++ pack.bat
@@ -0,0 +1,22 @@
+@echo EMPAQUETAR  
+IF "%~1"=="" GOTO ERROR1
+
+rmdir /s /q %1
+mkdir %1
+
+"C:\Program Files\7-Zip\7z.exe" a -r %1\cxp.zip ..\cxp\src\*
+"C:\Program Files\7-Zip\7z.exe" a -r %1\cxc.zip ..\cxc\src\*
+"C:\Program Files\7-Zip\7z.exe" a -r %1\compras.zip ..\compras\src\*
+"C:\Program Files\7-Zip\7z.exe" a -r %1\movinv.zip ..\movinv\src\*
+"C:\Program Files\7-Zip\7z.exe" a -r %1\webshell.zip ..\webshell\src\*
+"C:\Program Files\7-Zip\7z.exe" a -r %1\dkl-ddm.zip ..\dkl-ddm\src\*
+"C:\Program Files\7-Zip\7z.exe" a -r %1\dkl-web.zip ..\dkl-web\src\*
+"C:\Program Files\7-Zip\7z.exe" a -r %1\v12.zip ..\v12\src\*
+"C:\Program Files\7-Zip\7z.exe" a -r %1\dbext.zip ..\dbext\src\*
+GOTO EOF
+
+:ERROR1
+ECHO DEBE INDICAR UN DIRECTORIO DE SALIDA
+GOTO EOF
+
+:EOF
