c3b825e7caea80627297eac53f35ad3f80d652a4
Candelario <candelario.hernandez@induxsoft.net>
2023-12-27 09:50:27 -0600

    update

--- pack.bat
+++ pack.bat
@@ -13,6 +13,7 @@ mkdir %1
 "C:\Program Files\7-Zip\7z.exe" a -r %1\dkl-web.zip ..\dkl-web\src\*
 "C:\Program Files\7-Zip\7z.exe" a -r %1\v12.zip ..\v12\src\*
 "C:\Program Files\7-Zip\7z.exe" a -r %1\dbext.zip ..\dbext\src\*
+"C:\Program Files\7-Zip\7z.exe" a -r %1\catbas.zip ..\catbas\src\*
 GOTO EOF
 
 :ERROR1
