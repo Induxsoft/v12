1696e7d85bf7a2e778cf58ca0a67d92757df1791
Nepove <84743964+Nepove@users.noreply.github.com>
2023-12-28 14:33:34 -0600

    act

--- pack.bat
+++ pack.bat
@@ -13,6 +13,8 @@ mkdir %1
 "C:\Program Files\7-Zip\7z.exe" a -r %1\dkl-web.zip ..\dkl-web\src\*
 "C:\Program Files\7-Zip\7z.exe" a -r %1\v12.zip ..\v12\src\*
 "C:\Program Files\7-Zip\7z.exe" a -r %1\dbext.zip ..\dbext\src\*
+"C:\Program Files\7-Zip\7z.exe" a -r %1\pregop.zip ..\gop\src\*
+"C:\Program Files\7-Zip\7z.exe" a -r %1\mpi.zip ..\mpi\src\*
 "C:\Program Files\7-Zip\7z.exe" a -r %1\catbas.zip ..\catbas\src\*
 GOTO EOF
 
