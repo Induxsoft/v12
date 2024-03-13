826b50aa131c01115e047b39a94d9fd1ffbd639b
Nepove <84743964+Nepove@users.noreply.github.com>
2024-03-13 14:09:17 -0600

    Upd-archivo bat

--- pack.bat
+++ pack.bat
@@ -34,6 +34,7 @@ mkdir %1
 "C:\Program Files\7-Zip\7z.exe" a -r %1\sigesac.zip ..\sigesac\src\*
 "C:\Program Files\7-Zip\7z.exe" a -r %1\batch.zip ..\dkl-batch-api\src\*
 "C:\Program Files\7-Zip\7z.exe" a -r %1\stgt_geq.zip ..\STGT_guarderias2024\src\*
+"C:\Program Files\7-Zip\7z.exe" a -r %1\alibeb.zip ..\alibeb\src\*
 GOTO EOF
 
 :ERROR1
