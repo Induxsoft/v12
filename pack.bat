5a678daa617abfb708cd2899ffaf32dddd142685
Nepove <84743964+Nepove@users.noreply.github.com>
2024-04-11 14:51:42 -0600

    act bat

--- pack.bat
+++ pack.bat
@@ -36,6 +36,7 @@ mkdir %1
 "C:\Program Files\7-Zip\7z.exe" a -r %1\stgt_geq.zip ..\STGT_guarderias2024\src\*
 "C:\Program Files\7-Zip\7z.exe" a -r %1\alibeb.zip ..\alibeb\src\*
 "C:\Program Files\7-Zip\7z.exe" a -r %1\userext.zip ..\userext\src\*
+"C:\Program Files\7-Zip\7z.exe" a -r %1\orgcat.zip ..\orgcat\src\*
 GOTO EOF
 
 :ERROR1
