624d12fe85c2ea4d3807002d475aea772f2edcb4
Tiffani11 <62768118+EmmanuelME78@users.noreply.github.com>
2024-07-22 16:34:42 -0600

    Se agregó el archivo induxsoft.v12.ui.js para incluir código de propósito general al sistema, además se incluyeron funciones para redimensionar la "work_area" al alto necesario para que quepa sin barras de desplazamiento en la pantalla, esto puede verse en el estado de cuenta de cxc, solo es necesario que haya una work_area

---
+++ src/_templates/sites/v12/js/induxsoft.v12.ui.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(()=>{v12_on_resize();},200);
+});
+
+window.addEventListener("resize", function(event) {
+    v12_on_resize();
+}, true);
+
+function v12_on_resize()
+{
+    let const_h=24;
+    let work_area=document.getElementById("work_area");
+    if (!work_area) return;
+
+    let overflow=document.body.offsetHeight-window.innerHeight;
+    let h=work_area.offsetHeight-overflow-const_h;
+    work_area.style.height=h+"px";
+}
