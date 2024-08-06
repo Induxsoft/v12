9520705c1fc45633533051ed3a3ee3bd97e96259
Tiffani11 <62768118+EmmanuelME78@users.noreply.github.com>
2024-08-05 22:01:42 -0600

    Utilidades movidas al repositorio v12sdk

--- copywebcomponents.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Copiar JS Y CSS de Webcomponents
-copy ..\webcomponents\src\js\*.* .\src\_templates\sites\v12\js
-copy ..\webcomponents\src\css\*.* .\src\_templates\sites\v12\css
-copy ..\dkl-linux-fixes\pub\*.* .\src
-copy ..\dkl-linux-fixes\csv\csv\ref\CsvHelper.dll .\src
